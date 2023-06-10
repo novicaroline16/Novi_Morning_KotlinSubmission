7b423d50a2e2406c0efe09d8eca798bf52cf4c6c
novicaroline16 <carolinenovi16@gmail.com>
2023-06-11 00:43:08 +0800

    Update Exercise 4&5

--- src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
@@ -7,8 +7,20 @@ fun main() {
      *
      */
 
-    val name: String = "saya"
-
-
+    fun main() {
+        try {
+            // Potensi kode yang dapat menimbulkan exception
+            val angka1 = 10
+            val angka2 = 0
+            val hasil = angka1 / angka2
+            println("Hasil: $hasil")
+        } catch (e: ArithmeticException) {
+            // Penanganan exception khusus untuk ArithmeticException
+            println("Terjadi kesalahan aritmatika: ${e.message}")
+        } catch (e: Exception) {
+            // Penanganan exception umum untuk semua jenis exception lainnya
+            println("Terjadi kesalahan: ${e.message}")
+        }
+    }
 
 }
--- src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise5/App.kt
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise5/App.kt
@@ -8,7 +8,7 @@ fun main() {
      *
      */
 
-
+    val hero: Hero = Hero()
 
     /**
      * Latihan 2
@@ -16,16 +16,66 @@ fun main() {
      * Dan isi argumen-argumen / data dari parameter sesuai data diri Anda (Name, Age, Height)
      *
      */
+    class Hero {
+
+        private var name: String? = null
+        private var age = 20
+        private var height = 168
+
+        private var health = 5
+
+        fun setProfile(name: String?, age: Int, height: Int) {
+            this.name = name
+            this.age = age
+            this.height = height
+        }
+
+        fun profile() {
+            println("========= Profile =========")
+            println("Name: $name")
+            println("Age: $age")
+            println("Height: $height")
+            println("Health: $health")
+            println("===========================")
+        }
+
+        private fun checkHealth() {
+            if (health <= 0) {
+                println("Anda telah mati $name")
+            }
+        }
+
+        fun jalan() {
+            checkHealth()
+            println("$name sedang berjalan...")
+            health -= 1
+        }
 
 
 
-    /**
+        /**
      * Latihan 3
      * Tampilkan data profile dibawah ini, sebelum melakukan aktivitas
      *
      */
 
+        data class Profile(val group: Int, val jalan: Int, val lari: Int, val makan: Int, val minum: Int, val lompat: Int, val duduk: Int)
+        fun main() {
+            val profiles = listOf(
+                Profile(1, 5, 2, 3, 1, 3, 1),
+                Profile(2, 2, 2, 1, 3, 1, 2),
+                Profile(3, 3, 1, 5, 1, 5, 3),
+                Profile(4, 5, 4, 4, 4, 3, 2),
+                Profile(5, 4, 1, 3, 5, 5, 3),
+                Profile(6, 2, 4, 2, 2, 7, 4)
+            )
 
+            for (profile in profiles) {
+                println("Group ${profile.group}:")
+                println("Jalan ${profile.jalan}x, Lari ${profile.lari}x, Makan ${profile.makan}x, Minum ${profile.minum}x, Lompat ${profile.lompat}x, Duduk ${profile.duduk}x")
+                println()
+            }
+        }
 
     /**
     Challenge:
@@ -43,14 +93,75 @@ fun main() {
      *
      */
 
+    fun checkHealth(jalan: Int, makan: Int, minum: Int, lompat: Int, duduk: Int, lari: Int) {
+        val energy = jalan + makan + minum + lompat + duduk + lari
+        health = energy
+    }
+
+    }
 
 
 
-    /**
+/**
      * Latihan 5
      * Panggil fungsi profile dibawah ini untuk menampilkan status terkini setelah melakukan aktivitas
      *
      */
 
-
+fun profile(activity: String, count: Int) {
+    println("$activity: $count kali")
 }
+
+    fun main() {
+        println("Group 1:")
+        profile("Jalan", 5)
+        profile("Lari", 2)
+        profile("Makan", 3)
+        profile("Minum", 1)
+        profile("Lompat", 3)
+        profile("Duduk", 1)
+
+        println("Group 2:")
+        profile("Jalan", 2)
+        profile("Lari", 2)
+        profile("Makan", 1)
+        profile("Minum", 3)
+        profile("Lompat", 1)
+        profile("Duduk", 2)
+
+        println("Group 3:")
+        profile("Jalan", 3)
+        // "Lari Ix" mungkin terjadi kesalahan penulisan, diganti menjadi "Lari 1x"
+        profile("Lari", 1)
+        profile("Makan", 5)
+        profile("Minum", 1)
+        profile("Lompat", 5)
+        profile("Duduk", 3)
+
+        println("Group 4:")
+        profile("Jalan", 5)
+        profile("Lari", 4)
+        profile("Makan", 4)
+        profile("Minum", 4)
+        profile("Lompat", 3)
+        profile("Duduk", 2)
+
+        println("Group 5:")
+        profile("Jalan", 4)
+        profile("Lari", 1)
+        profile("Makan", 3)
+        profile("Minum", 5)
+        profile("Lompat", 5)
+        profile("Duduk", 3)
+
+        println("Group 6:")
+        profile("Jalan", 2)
+        profile("Lari", 4)
+        profile("Makan", 2)
+        profile("Minum", 2)
+        profile("Lompat", 7)
+        profile("Duduk", 4)
+
+    }
+
+
